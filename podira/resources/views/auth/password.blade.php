655916efd2cc4d35c33e2bf55bbcc8f507260d0d
Edward Yu <edwardyu@yahoo.com>
2015-08-26 19:06:58 -0500

    auth

---
+++ podira/resources/views/auth/password.blade.php
@@ -0,0 +1,16 @@
+<!-- resources/views/auth/password.blade.php -->
+
+<form method="POST" action="/password/email">
+    {!! csrf_field() !!}
+
+    <div>
+        Email
+        <input type="email" name="email" value="{{ old('email') }}">
+    </div>
+
+    <div>
+        <button type="submit">
+            Send Password Reset Link
+        </button>
+    </div>
+</form>
