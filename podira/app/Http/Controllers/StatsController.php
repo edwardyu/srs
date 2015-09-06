0e842a7bb0d39b3cd6f448f57828ae350be6fc95
Edward Yu <edwardyu@yahoo.com>
2015-09-05 23:48:07 -0500

    stats view

--- podira/app/Http/Controllers/StatsController.php
+++ podira/app/Http/Controllers/StatsController.php
@@ -17,7 +17,7 @@ class StatsController extends Controller
         $this->middleware('auth');
         $this->middleware('auth.deck.edit', ['only' => ['deckStats']]);
     }
-    
+
     /**
      * Show the statistics for the entire deck. Can only be accessed if the user has edit permissions on the deck.
      * @param Request $request - a HTTP request 
@@ -25,6 +25,40 @@ class StatsController extends Controller
      */
     public function deckStats(Request $request, $id)
     {
+        $deck = \App\Deck::find($id);
+        $deckStatsCalculator = new \App\Stats\DeckStatsCalculator($deck);
+        $numUsers = $deckStatsCalculator->numUsers();
+        $totalTime = $deckStatsCalculator->totalTime();
+        $totalInteractions = $deckStatsCalculator->totalInteractions();
+        $mostDifficultConcepts = $deckStatsCalculator->mostDifficultConcepts();
+        $mostIntuitiveConcepts = $deckStatsCalculator->mostIntuitiveConcepts();
+        $mostDifficultCards = $this->convertArrayKeysToFlashcards($mostDifficultConcepts);
+        $mostIntuitiveCards = $this->convertArrayKeysToFlashcards($mostIntuitiveConcepts);
 
+        return view('stats.deck')->with([
+            'deck' => $deck,
+            'numUsers' => $numUsers,
+            'totalTime' => $totalTime,
+            'totalInteractions' => $totalInteractions,
+            'mostDifficultConcepts' => $mostDifficultConcepts,
+            'mostIntuitiveConcepts' => $mostIntuitiveConcepts,
+            'mostDifficultCards' => $mostDifficultCards,
+            'mostIntuitiveCards' => $mostIntuitiveCards
+        ]);
+    }
+
+    /**
+     * Convert the keys of an array which are flashcard ids to actual flashcard objects.
+     */
+    private function convertArrayKeysToFlashcards(array $array)
+    {
+        $flashcards = [];
+
+        foreach($array as $key => $value)
+        {
+            $flashcards[] = \App\Flashcard::find((int) $key);
+        }
+
+        return $flashcards;
     }
 }
