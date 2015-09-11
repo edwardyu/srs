0e601ffb0efb928a4be33d5bbe8fd879f888744d
Edward Yu <edwardyu@yahoo.com>
2015-09-11 16:02:07 -0400

    load sat words in

--- podira/app/Scripts/SATLoader.php
+++ podira/app/Scripts/SATLoader.php
@@ -7,8 +7,11 @@ namespace App\Scripts;
 
 class SATLoader
 {
+	private $deck;
+
 	public function load()
 	{
+		$this->deck = \App\Deck::create(['name' => 'Elite SAT Vocabulary']);
 		$handle = fopen(storage_path() . '/app/Elite SAT Vocabulary.txt', 'r');
 		while($line = fgets($handle)) {
 			$this->handleLine($line);
@@ -20,5 +23,7 @@ class SATLoader
 		$wordArray = explode("\t", $line);
 		$front = $wordArray[0];
 		$back = $wordArray[1];
+		$flashcard = new \App\Flashcard(['front' => $front, 'back' => $back]);
+		$this->deck->flashcards()->save($flashcard);
 	}
 }
