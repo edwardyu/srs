7f5a8e9889a6aaba90b100ad2daea4ad3cf87a44
Edward Yu <edwardyu@yahoo.com>
2015-09-15 01:48:52 -0400

    change homepage depending on if logged in

--- podira/app/Http/Controllers/Auth/PasswordController.php
+++ podira/app/Http/Controllers/Auth/PasswordController.php
@@ -17,6 +17,7 @@ class PasswordController extends Controller
     | explore this trait and override any methods you wish to tweak.
     |
     */
+    protected $redirectTo = '/';
 
     use ResetsPasswords;
 
