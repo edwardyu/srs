1dd408b06ce27d6a6450e2fef1526cdb16a2f376
Edward Yu <edwardyu@yahoo.com>
2015-09-17 20:58:57 -0400

    add google analytics

--- podira/resources/views/includes/footer.blade.php
+++ podira/resources/views/includes/footer.blade.php
@@ -1,1 +1,11 @@
 <div style="width:100%;height:50px;color:white;font-family:Lato;font-size:11px;text-align:center;padding-top:19px;box-sizing:border-box;" class="bgmatte">All Rights Reserved</div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-43007738-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
