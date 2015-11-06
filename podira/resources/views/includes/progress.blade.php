5dde64c59250b13280556ac7f1bfaa4b3a928197
Mathew <mdp2163@columbia.edu>
2015-11-06 18:33:15 -0500

    fixing divide by zeor bug

--- podira/resources/views/includes/progress.blade.php
+++ podira/resources/views/includes/progress.blade.php
@@ -1,11 +1,14 @@
 <div class="progress bgpurple">
-    <h1>PROGRESS {{count($totalFlashcards) - $remainingFlashcards + 1}}/{{count($totalFlashcards)}}
-
+    <h1>PROGRESS
+      @if(count($totalFlashcards) != 0)
+      {{count($totalFlashcards) - $remainingFlashcards + 1}}/{{count($totalFlashcards)}}
+      @endif
 
     </h1>
     <div class="progressbar">
       <div class="startdiode bgpurple"></div>
       @foreach($totalFlashcards as $index)
+        @if(count($totalFlashcards) != 0)
           @if($index > (count($totalFlashcards) - $remainingFlashcards + 1))
           <div class="diode" style="left:{{ (100 / count($totalFlashcards)) * $index }}%;
           left: calc({{ (100 / count($totalFlashcards)) * $index }}% - 10px);"></div>
@@ -13,10 +16,12 @@
           <div class="diode bgpurple" style="left:{{ (100 / count($totalFlashcards)) * $index }}%;
           left: calc({{ (100 / count($totalFlashcards)) * $index }}% - 10px);"></div>
           @endif
+        @endif
       @endforeach
       <div class="enddiode"></div>
-
+      @if(count($totalFlashcards) != 0)
       <div class="innerbar bgpurple" style="width: {{ (100 / count($totalFlashcards)) * (count($totalFlashcards) - $remainingFlashcards + 1) }}%"></div>
+      @endif
 
     </div>
 </div>
