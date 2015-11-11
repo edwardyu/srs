300e67f34831665ce9bfc6b7a89e2dec7708333b
Mathew <mdp2163@columbia.edu>
2015-11-10 22:58:14 -0800

    fixing reset password view

--- podira/resources/views/card/create.blade.php
+++ podira/resources/views/card/create.blade.php
@@ -23,32 +23,4 @@
 				</select>
 				<input type="submit" value="Create Deck">
 		</form>
-
-
-
 </section>
-<!--
-<html>
-	<head>
-		<title>Create a flashcard.</title>
-	</head>
-	<body>
-		<h1>Create a flashcard.</h1>
-		<form method="POST" action="/card/store">
-		    {!! csrf_field() !!}
-		    <div>
-		        Front
-		        <input type="text" name="front" value="{{ old('front') }}">
-		    </div>
-
-		    <div>
-		        Back
-		        <input type="text" name="back" value="{{ old('back') }}">
-		    </div>
-
-		    <div>
-		        <button type="submit">Create</button>
-		    </div>
-		</form>
-	</body>
-</html>-->
--- podira/resources/views/emails/password.blade.php
+++ podira/resources/views/emails/password.blade.php
@@ -1,3 +1,6 @@
 <!-- resources/views/emails/password.blade.php -->
-
+@extends('layouts.master')
+@section('title', 'Reset Password')
+@section('content')
 Click here to reset your password: {{ url('password/reset/'.$token) }}
+@endsection
